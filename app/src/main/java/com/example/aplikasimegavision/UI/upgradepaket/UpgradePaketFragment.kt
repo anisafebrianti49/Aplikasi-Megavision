2a1f1841b221051e1b09449c2a761fae87703f08
egaslfhy <egaaaa232@gail.com>
2026-07-08 16:33:33 +0700

    fix button error

---
+++ app/src/main/java/com/example/aplikasimegavision/UI/upgradepaket/UpgradePaketFragment.kt
@@ -0,0 +1,27 @@
+package com.example.aplikasimegavision.UI.upgradepaket
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.ui.platform.ComposeView
+import androidx.fragment.app.Fragment
+
+class UpgradePaketFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return ComposeView(requireContext()).apply {
+            setContent {
+                UpgradePaketScreen(
+                    onBackClicked = {
+                        parentFragmentManager.popBackStack()
+                    }
+                )
+            }
+        }
+    }
+}
